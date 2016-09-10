00ac7f191b4d3398fee41254ec0f6b84b5c754d7
Kenneth Korcal <kakorcal@gmail.com>
2016-09-10 00:27:47 -0700

    adding mySolutions directory

---
+++ mySolutions/chapter1/ch1-q1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Keep track of seen characters with a Set data structure, fail when
+ * a repeated character is found.
+ *
+ * Time: O(N)
+ * Additional space: O(N)
+ *
+ * @param  {string[]} str String to check, passed in as a character array
+ * @return {boolean}      True if unique characters, otherwise false
+ */
+export function hasUniqueCharactersSet(str) {
+}
+
+/**
+ * Sort the original string first then iterate through it. Repeat characters
+ * will show up next to eachother so fail if any two characters in a row
+ * are the same.
+ *
+ * Time: O(N lg N)
+ * Additional space: O(1)
+ *
+ * @param  {string[]} str String to check, passed in as a character array
+ * @return {boolean}      True if unique characters, otherwise false
+ */
+export function hasUniqueCharactersSort(str) {
+}
